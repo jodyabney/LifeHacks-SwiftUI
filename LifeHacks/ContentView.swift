32bebfb47fc200f14c8a92524cf254ac7ef03321
Abney Services LLC <jody@abneyservices.com>
2022-01-17 07:51:49 -0500

    Initial Commit

---
+++ LifeHacks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LifeHacks
+//
+//  Created by Jody Abney on 1/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LifeHacks/LifeHacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeHacksApp.swift
+//  LifeHacks
+//
+//  Created by Jody Abney on 1/17/22.
+//
+
+import SwiftUI
+
+@main
+struct LifeHacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
